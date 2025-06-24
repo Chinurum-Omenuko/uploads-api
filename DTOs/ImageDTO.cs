1cec00d9ab84fa06e6dab854d3a440d5884530aa
Chinurum-Omenuko <omen0003@algonquinlive.com>
2025-06-24 13:56:19 -0400

    adda Blob storage package

--- DTOs/ImageDTO.cs
+++ DTOs/ImageDTO.cs
@@ -9,6 +9,7 @@ namespace uploads_api.DTOs
     {
         public string Id { get; set; }
         public string URL { get; set; }
+        public byte[] ImageData { get; set; }
         public string AltText { get; set; }
         public string UploadedAt { get; set; }
     }
