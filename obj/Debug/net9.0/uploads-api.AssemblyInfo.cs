6b4b9946a5b7632e086ea249859172d64e2afa4c
Chinurum-Omenuko <omen0003@algonquinlive.com>
2025-06-29 16:15:52 -0400

    assembly cache

--- obj/Debug/net9.0/uploads-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/uploads-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("uploads-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d580c6eb108f0efdf98611563f29bc5565808d03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dd831f627e4085bad5e7f9b7b5f3faf4d6f1715")]
 [assembly: System.Reflection.AssemblyProductAttribute("uploads-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("uploads-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
